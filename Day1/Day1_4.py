e9dfb1bae1a872a07e44be078402f9477cf6b22c
tommehgg <tommy_broekman@outlook.com>
2022-04-02 21:39:08 +0200

    Day 1 done

---
+++ Day1/Day1_4.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+backup_a = a
+backup_b = b
+b = backup_a
+a = backup_b
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
