42c9c8e373c7df1ff0244b08344c27e7b9d11ba8
tommehgg <tommy_broekman@outlook.com>
2022-04-03 22:38:25 +0200

    almost done with Day 2

---
+++ Day2/Day2_1.py
@@ -0,0 +1,18 @@
+# Day 2 - Tommy Broekman
+
+
+#Data Types
+
+
+#String
+print("Hello"[4])
+print("123"+"345")
+#integer 
+print(123 + 345)
+#float
+3.14159
+#boolean
+True
+False
+
+
---
+++ Day2/Day2_2.py
@@ -0,0 +1,12 @@
+num_char = len(input('What is your name? '))
+
+print(type(num_char))
+
+num_char_changed = str(num_char)
+
+print("Your name has "+ num_char_changed + " characters")
+
+a = 123
+print(type(a))
+convert_a = str(a)
+print(type(convert_a))
